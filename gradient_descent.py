e37da08cb90682becd35f05022fe9c0fff125720
Gitdres <andresisakhlynsson@gmail.com>
2025-11-21 14:49:21 +0100

    new gradient descent function

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,41 +1,29 @@
+from TrainingFunctions import backwards
+from LossFunctions import MeanSquaredError, MeanSquaredErrorDerivative
 import numpy as np
 
-#Basic gradient descent
+def train_sgd(network, inputs, targets, epochs, learning_rate):
+    
+    
+    
+    # inputs:  shape (n_features, n_samples)
+    # targets: shape (n_outputs, n_samples)
+    
+    n_samples = inputs.shape[1]
 
-def gradient_descent(parameters, gradients, learning_rate):
-    """
-    parameters: dict of parameters (W1, b1, W2, b2, ...)
-    gradient: dict of corresponding gradients (dW1, db1, ...)
-    """
-    for name in params:
-        parameters[name] -= learning_rate * gradients[name]
+    for epoch in range(epochs):
+        # shuffle sample order
+        indices = np.random.permutation(n_samples)
 
+        for idx in indices:
+            # extracting just one sample
+            input_sample  = inputs[:,  idx:idx+1]   # (n_features, 1)
+            target_sample = targets[:, idx:idx+1]   # (n_outputs, 1)
 
-# Stochastic gradient descent
+            # computing gradients
+            grad_W = backwards(network, input_sample, target_sample,
+                                MeanSquaredErrorDerivative)
 
-def iterate_minibatches(X, y, batch_size, shuffle=True):
-    """
-    Yield mini-batches of (X_batch, y_batch).
-
-    X: array of shape (N, D)
-    y: array of shape (N,) or (N, C)
-    """
-    N = X.shape[0]
-    indices = np.arange(N)
-    if shuffle:
-        np.random.shuffle(indices)
-
-    for start in range(0, N, batch_size):
-        end = start + batch_size
-        batch_idx = indices[start:end]
-        yield X[batch_idx], y[batch_idx]
-
-def sgd_update(params, grads, learning_rate):
-    """
-    Perform one SGD parameter update.
-
-    params: dict of parameter arrays (e.g. {"W1": W1, "b1": b1, ...})
-    grads:  dict of gradient arrays with same keys ({"W1": dW1, "b1": db1, ...})
-    """
-    for name in params:
-        params[name] -= learning_rate * grads[name]
+            # gradient descent step
+            for i in range(len(network.weights_list)):
+                network.weights_list[i] -= learning_rate * grad_W[i]
