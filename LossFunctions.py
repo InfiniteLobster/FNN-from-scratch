83f1dfc4a652c272396415966f3327455794c54a
InfiniteLobster <szcholewinski@gmail.com>
2025-12-04 16:54:39 +0100

    Finished commenting/fixing half of loss functions

--- LossFunctions.py
+++ LossFunctions.py
@@ -6,24 +6,56 @@ import numpy as np
 # Mean Squared Error (MSE)
 # ================================================
 def MeanSquaredError(targets, predictions):
+    #getting the difference between ground truth and predictions
     diff = predictions - targets
-    return 0.5 * np.sum(diff ** 2)
+    #squaring the difference
+    sqr = diff**2
+    #getting mean of squared values
+    mean = np.mean(sqr)
+    #putting results into output variable
+    output = mean
+    #retunring results
+    return output
 
 def MeanSquaredErrorDerivative(targets, predictions):
-    return (predictions - targets)
+    #getting number of examples for proper division
+    num_examples = predictions.shape[1]
+    #getting the difference between ground truth and predictions
+    diff = predictions - targets
+    #multiplying by 2 
+    mult = 2 * diff
+    #dividing results with number of examples (averaging)
+    div = mult/num_examples
+    #putting results into output variable
+    output = div
+    #retunring results
+    return output
 # ================================================
 # Binary Cross-Entropy (for sigmoid output)
 # ================================================
 def BinaryCrossEntropy(targets, predictions, eps=1e-12):
-    #
+    #predictions are clipped for numerical stability
     predictions = np.clip(predictions, eps, 1 - eps)
-    #
-    return -np.mean(targets * np.log(predictions) +
-                    (1 - targets) * np.log(1 - predictions))
+    #calculating components separatly for code clarity
+    first_component = targets * np.log(predictions)
+    second_component = (1 - targets) * np.log(1 - predictions)
+    #adding components and averaging them 
+    avg = np.mean(first_component + second_component)
+    #assigning avg results to variable (plus negating it)
+    output = -avg            
+    #returning results
+    return output
 
 def BinaryCrossEntropyDerivative(targets, predictions, eps=1e-12):
-    predictions = np.clip(predictions, eps, 1 - eps)
-    return (predictions - targets) / (predictions * (1 - predictions))
+    #predictions are clipped for numerical stability
+    predictions = np.clip(predictions, eps, 1 - eps) 
+    #calculating components separatly for code clarity
+    first_component = (predictions - targets)
+    second_component = (predictions * (1 - predictions))
+    #dividing components to reach results
+    output = first_component / second_component
+    #returning results
+    return output
 #Cross-Entropy
 def CrossEntropy(targets, predictions):
     #
